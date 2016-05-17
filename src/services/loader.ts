979c92b6788e4617603942fe24b2f7fc0b595f9c
Karel Hala <ch4rlien@gmail.com>
2016-05-17 22:10:51 +0200

    Update loader.ts
    Add loadeing of jobs and school loader services

--- src/services/loader.ts
+++ src/services/loader.ts
@@ -2,9 +2,13 @@
 import BasicInformationLoader from './basicInformationLoader';
 import TimelineLoader from './timelineLoader';
 import ContactLoader from './contactLoader';
+import JobsLoader from './jobsLoader';
+import SchoolLoader from './schoolLoader';
 
 export default (module: ng.IModule) => {
   module.service('basicInformationLoader', BasicInformationLoader);
   module.service('timelineLoader', TimelineLoader);
   module.service('contactLoader', ContactLoader);
+  module.service('jobsLoader', JobsLoader);
+  module.service('schoolLoader', SchoolLoader);
 }
